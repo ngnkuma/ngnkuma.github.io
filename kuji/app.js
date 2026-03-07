a83e543205a5892224279db929f6a681962ec7a3
ngnkuma <kndsh@naver.com>
2026-03-08 01:29:15 +0900

    Add files via upload

--- kuji/app.js
+++ kuji/app.js
@@ -709,6 +709,9 @@ function setupEvents() {
     }
 }
 
+
+/* 모바일 확대 방지 */
+
 document.documentElement.addEventListener('touchstart', function (event) {
      if (event.touches.length > 1) {
           event.preventDefault(); 
@@ -719,7 +722,7 @@ var lastTouchEnd = 0;
 
 document.documentElement.addEventListener('touchend', function (event) {
      var now = (new Date()).getTime();
-     if (now - lastTouchEnd <= 300) {
+     if (now - lastTouchEnd <= 200) {
           event.preventDefault(); 
         } lastTouchEnd = now; 
     }, false);
